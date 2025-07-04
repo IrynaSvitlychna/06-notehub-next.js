77435a4d5ac6b08c591cdd7e0fcf7052e0678daa
irinasvetlichnaya <irinasvetlichnaya16@gmail.com>
2025-07-04 16:31:17 +0300

    changed description

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
     <div className={css.container}>
       <h1 className={css.title}>Welcome to NoteHub</h1>
       <p className={css.description}>
-        NoteHub is a simple and efficient application designed for managing
+      NoteHub is a simple and efficient application designed for managing
         personal notes. It helps keep your thoughts organized and accessible
-        in one place, whether you're at home or on the go.
+        in one place, whether you&aposre at home or on the go.
       </p>
       <p className={css.description}>
         The app provides a clean interface for writing, editing, and browsing
